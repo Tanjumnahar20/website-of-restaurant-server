dff7f89cbaf6716461f155d121a8707803d7e099
Tanjum Nahar <tanjumnaharchandana@gmail.com>
2024-06-06 13:53:53 +0600

    payment added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
+const stripe = require('stripe')(process.env.PAYEMENT_SECRET_KEY)
 
 const port = process.env.PORT || 5000;
 
@@ -15,6 +16,7 @@ console.log(process.env.res_USER);
 
 
  const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const { default: Stripe } = require('stripe');
  const uri =`mongodb+srv://${process.env.res_USER}:${process.env.res_PASS}@cluster0.7ijeqqy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
  
  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,14 +34,87 @@ console.log(process.env.res_USER);
        await client.connect();
        
        const menuCollection = client.db('resDb').collection('menu');
+       const paymentCollection = client.db('resDb').collection(' payment');
        const userCollection = client.db('resDb').collection('users');
        const reviewCollection = client.db('resDb').collection('review');
        const cartCollection = client.db('resDb').collection('carts');
+      
+
+          // middleware for token verify______
+     const verifyToken=(req,res,next)=>{
+      console.log('inside verify token', req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message:'forbidden access'})
+      }
+      const token = req.headers.authorization.split(" ")[1];
+       jwt.verify(token,process.env.TOKEN_SECRET, (err,decoded)=>{
+        if(err){
+          return res.status(401).send({message:'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+       })
+     }
+  // only admin can use apii______(admin or not check)
+     const verifyAdmin = async(req,res,next)=>{
+      const email = req.decoded.email;
+      const query = {email: email}
+      const user = await userCollection.findOne(query);
+      isAdmin = user?.role==='admin'
+      if(!isAdmin){
+         return res.status(401).send({message: 'unauthorized access'})
+      }
+      next();
+     }
+
+
 
        app.get('/menu', async(req,res)=>{
          const result = await menuCollection.find().toArray();
          res.send(result);
        })
+
+      // apii for____: admin add  new data to the db
+      app.post('/menu',verifyToken,verifyAdmin, async(req,res)=>{
+        const item = req.body;
+        const result = await menuCollection.insertOne(item);
+        res.send(result);
+      })
+
+      // api for admin delete menu 
+      app.delete('/menu/:id', verifyToken, verifyAdmin, async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId (id)};
+        const result = await menuCollection.deleteOne(query);
+        res.send(result);
+      })
+
+      // api for update menu item(admin)
+      app.get('/menu/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await menuCollection.findOne(query)
+        res.send(result);
+      })
+
+      // api for update menu (admin)
+      app.patch('/menu/:id', async(req,res)=>{
+        const item = req.body;
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const updateDoc ={
+          $set:{
+            name: item.name,
+            price: item.price,
+            category: item.category,
+            image: item.image,
+            recipe: item.recipe
+
+          }
+        }
+        const result = await menuCollection.updateOne(filter, updateDoc);
+        res.send(result)
+      })
 
        app.get('/review', async(req,res)=>{
          const result = await reviewCollection.find().toArray();
@@ -84,32 +159,7 @@ console.log(process.env.res_USER);
       })
 
     
-      // middleware for token verify______
-     const verifyToken=(req,res,next)=>{
-      console.log('inside verify token', req.headers.authorization);
-      if(!req.headers.authorization){
-        return res.status(401).send({message:'forbidden access'})
-      }
-      const token = req.headers.authorization.split(" ")[1];
-       jwt.verify(token,process.env.TOKEN_SECRET, (err,decoded)=>{
-        if(err){
-          return res.status(401).send({message:'forbidden access'})
-        }
-        req.decoded = decoded;
-        next();
-       })
-     }
-  // only admin can use apii______(admin or not check)
-     const verifyAdmin = async(req,res,next)=>{
-      const email = req.decoded.email;
-      const query = {email: email}
-      const user = await userCollection.findOne(query);
-      isAdmin = user?.role==='admin'
-      if(!isAdmin){
-         return res.status(401).send({message: 'unauthorized access'})
-      }
-      next();
-     }
+   
 
 
 
@@ -163,6 +213,39 @@ console.log(process.env.res_USER);
         expiresIn: '1hr'
       })
       res.send({token});
+    })
+
+
+    // PAYMENT RELATED APIII________
+    app.post('/create-payment-intent', async(req,res)=>{
+      const {price} =req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+    // save payment info to the database
+    app.post('/payment', async(req,res)=>{
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+      console.log('payemnet', payment);
+      
+      // carefully delete each item to the card
+     const query = {_id: {
+      $in: payment. menuItemId.map( id =>new ObjectId(id))
+     }}
+
+     const deleteResult = await cartCollection.deleteMany(query)
+
+      res.send({paymentResult,deleteResult})
+
     })
 
 
